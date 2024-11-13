3a9ca728133766ba6d097a209a6b85cb40b72c12
Flambé <emilycogsdill@gmail.com>
2024-11-13 16:18:21 -0700

    set up vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RedditForm from "./RedditForm";
+
+function App() {
+    return (
+        <div>
+            <RedditForm />
+        </div>
+    );
+}
+
+export default App;
